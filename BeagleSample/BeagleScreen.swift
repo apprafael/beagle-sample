266fbe7f91cbb7ad2a6fc4ba8a8191d9ef066921
rafael <rafael.almeida@zup.com.br>
2020-07-20 10:07:27 -0300

    beagle overview screen

--- BeagleSample/BeagleScreen.swift
+++ BeagleSample/BeagleScreen.swift
@@ -14,7 +14,12 @@ class BeagleScreen {
         return Screen(
             child: Container(
                 children: [
-                    Text("Hello Beagle!")
+                    Image(.value(.remote(.init(url: "https://uploads-ssl.webflow.com/5cac6236f8d44ddee118d97c/5ea723abad4ab09a53fa351d_Beagle-azul-p-500.png")))),
+                    Text("Framework que atualiza UI via backend"),
+                    Text("O Beagle é uma plataforma Server-Driven para iOS, Android e Web que facilita a construção de telas por meio de um backend, permitindo que telas e fluxos nativos sejam alterados utilizando apenas JSON."),
+                    
+                    Button(text: "Download", onPress: [Navigate.openExternalURL("https://docs.usebeagle.io/primeiros-passos/instalando-beagle")]),
+                    Button(text: "Comece agora", onPress: [Navigate.openExternalURL("https://docs.usebeagle.io")])
                 ]
             ).applyFlex(
                 Flex(
